841469c7520c715abb065589f9181ebb06c085f4
Laura Luiz <laura.luiz@commercetools.de>
2015-11-27 11:09:26 +0100

    Improve example

--- src/main/java/handson/Main.java
+++ src/main/java/handson/Main.java
@@ -1,10 +1,8 @@
 package handson;
 
-import io.sphere.sdk.carts.Cart;
-import io.sphere.sdk.carts.CartDraft;
-import io.sphere.sdk.carts.CustomLineItemDraft;
-import io.sphere.sdk.carts.LineItem;
+import io.sphere.sdk.carts.*;
 import io.sphere.sdk.carts.commands.CartCreateCommand;
+import io.sphere.sdk.carts.commands.CartDeleteCommand;
 import io.sphere.sdk.carts.commands.CartUpdateCommand;
 import io.sphere.sdk.carts.commands.updateactions.AddCustomLineItem;
 import io.sphere.sdk.carts.commands.updateactions.AddLineItem;
@@ -17,12 +15,15 @@ import io.sphere.sdk.client.SphereRequest;
 import io.sphere.sdk.commands.UpdateAction;
 import io.sphere.sdk.models.DefaultCurrencyUnits;
 import io.sphere.sdk.models.LocalizedString;
+import io.sphere.sdk.models.Reference;
 import io.sphere.sdk.products.ProductProjection;
+import io.sphere.sdk.products.queries.ProductProjectionByIdGet;
 import io.sphere.sdk.products.queries.ProductProjectionQuery;
 import io.sphere.sdk.queries.PagedQueryResult;
+import io.sphere.sdk.taxcategories.TaxCategory;
 import io.sphere.sdk.types.*;
 import io.sphere.sdk.types.commands.TypeCreateCommand;
-import io.sphere.sdk.types.queries.TypeQuery;
+import io.sphere.sdk.types.commands.TypeDeleteCommand;
 import org.javamoney.moneta.Money;
 
 import java.io.IOException;
@@ -30,6 +31,7 @@ import java.math.BigDecimal;
 import java.util.*;
 
 import static io.sphere.sdk.models.TextInputHint.*;
+import static io.sphere.sdk.products.ProductProjectionType.CURRENT;
 import static io.sphere.sdk.utils.SetUtils.asSet;
 import static java.util.Arrays.asList;
 import static java.util.Locale.ENGLISH;
@@ -37,7 +39,8 @@ import static java.util.Locale.ENGLISH;
 public class Main {
 
     public static final String SKU = "book-sku"; // TODO set a real sku in your project which has a tax category!
-    public static final String CUSTOM_TYPE_KEY = "sap-type";
+
+    public static final String CUSTOM_TYPE_KEY = "sap-type3";
     public static final String PRODUCT_ID_FIELD = "productId";
     public static final String PRODUCT_SLUG_FIELD = "productSlug";
     public static final String PRODUCT_SKU_FIELD = "productSku";
@@ -51,39 +54,79 @@ public class Main {
 
         try(final SphereClient client = SphereClientFactory.of().createClient(clientConfig)) {
             // Done only once at creation of the project
-            createTypeForExtendedCustomLineItem(client);
+            final Type typeForCustomLineItems = createTypeForExtendedCustomLineItem(client);
+            System.err.println("Created typeForCustomLineItems for extending custom line items with product information:");
+            System.err.println(typeForCustomLineItems);
 
+            // We create a cart at some moment, e.g. when the customer logs in
             final Cart cart = createCart(client);
-            final ProductProjection product = getSomeProduct(client);
+            System.err.println("");
+            System.err.println("Empty cart created:");
+            System.err.println(cart);
 
+            // When clicked on "add to cart" we add a product to the cart, which contains a non-customized price
             final int quantity = 3;
-            final Cart cartWithProductAndPrice = addLineItemWithPriceFromSap(client, product, cart, quantity);
+            final ProductProjection product = getSomeProduct(client);
+            final Cart cartWithLineItem = addProductToCart(client, cart, product, quantity);
+            System.err.println("");
+            System.err.println("Cart with regular line items:");
+            System.err.println(cartWithLineItem);
+
+            // When clicked on "refresh price" we replace the line items with custom line items, which contain the price from SAP
+            final Cart cartWithProductAndPrice = updateCartWithCustomPrices(client, cartWithLineItem);
+            System.err.println("");
+            System.err.println("Cart with line items with SAP price:");
             System.err.println(cartWithProductAndPrice);
+
+            // Clean up project to avoid conflicts for next iteration
+            cleanUpProject(client, typeForCustomLineItems, cartWithProductAndPrice);
         }
     }
 
-    private static Cart addLineItemWithPriceFromSap(final SphereClient client, final ProductProjection product,
-                                                    final Cart cart, final int quantity) {
-        final Cart cartWithLineItem = addProductToCart(client, cart, product, quantity);
-        final Cart cartWithCustomLineItem = replaceLineItemWithCustomLineItem(client, product, cartWithLineItem);
-        return addProductInfoFieldsInCustomLineItem(client, product, cartWithCustomLineItem);
+    private static Cart updateCartWithCustomPrices(final SphereClient client, final Cart cart) {
+        final Map<String, Map<String, Object>> lineItemInfo = new HashMap<>();
+
+        // Replace line item for custom line item
+        final List<UpdateAction<Cart>> replaceUpdateActions = new ArrayList<>();
+        for (final LineItem lineItem : cart.getLineItems()) {
+            replaceUpdateActions.addAll(actionToReplaceLineItemWithCustomLineItem(client, lineItem));
+            lineItemInfo.put(lineItem.getId(), extractInfoFromLineItem(lineItem));
+        }
+        final Cart cartWithCustomLineItems = execute(client, CartUpdateCommand.of(cart, replaceUpdateActions));
+
+        // Update custom line item with product info
+        final List<UpdateAction<Cart>> updateProductInfoActions = new ArrayList<>();
+        for (final CustomLineItem customLineItem : cartWithCustomLineItems.getCustomLineItems()) {
+            final Map<String, Object> productInfo = lineItemInfo.get(customLineItem.getSlug());
+            updateProductInfoActions.add(actionToAddProductInfoFieldsInCustomLineItem(customLineItem, productInfo));
+        }
+        return execute(client, CartUpdateCommand.of(cartWithCustomLineItems, updateProductInfoActions));
     }
 
-    private static Cart addProductInfoFieldsInCustomLineItem(final SphereClient client, final ProductProjection product, final Cart cartWithCustomLineItem) {
-        final Map<String, Object> values = new HashMap<>();
-        values.put(PRODUCT_ID_FIELD, product.getId());
-        values.put(PRODUCT_SLUG_FIELD, product.getSlug());
-        values.put(PRODUCT_SKU_FIELD, product.getMasterVariant().getSku());
-        final SetCustomLineItemCustomType setCustomLineItemCustomType = SetCustomLineItemCustomType
-                .ofTypeKeyAndObjects(CUSTOM_TYPE_KEY, values, getCustomLineItemInCart(cartWithCustomLineItem));
-        return execute(client, CartUpdateCommand.of(cartWithCustomLineItem, setCustomLineItemCustomType));
+    private static List<UpdateAction<Cart>> actionToReplaceLineItemWithCustomLineItem(final SphereClient client, final LineItem lineItem) {
+        final Reference<TaxCategory> taxCategory = obtainTaxCategoryFromProduct(client, lineItem);
+        final CustomLineItemDraft customLineItemDraft = CustomLineItemDraft.of(lineItem.getName(), lineItem.getId(), getFinalPriceFromSap(), taxCategory, lineItem.getQuantity());
+        return asList(AddCustomLineItem.of(customLineItemDraft), RemoveLineItem.of(lineItem));
     }
 
-    private static Cart replaceLineItemWithCustomLineItem(final SphereClient client, final ProductProjection product, final Cart cart) {
-        final LineItem lineItemInCart = getLineItemInCart(cart);
-        final CustomLineItemDraft customLineItemDraft = CustomLineItemDraft.of(product.getName(), "product-sap", getFinalPriceFromSap(), product.getTaxCategory(), lineItemInCart.getQuantity());
-        final List<UpdateAction<Cart>> updateActions = asList(AddCustomLineItem.of(customLineItemDraft), RemoveLineItem.of(lineItemInCart));
-        return execute(client, CartUpdateCommand.of(cart, updateActions));
+    private static Reference<TaxCategory> obtainTaxCategoryFromProduct(final SphereClient client, final LineItem lineItem) {
+        // This information is not currently provided by the line item, so it has to be fetched from the product
+        // It would make sense that this is provided by the line item so in the future this might be not necessary
+        final ProductProjection product = execute(client, ProductProjectionByIdGet.of(lineItem.getProductId(), CURRENT));
+        return product.getTaxCategory();
+    }
+
+    private static UpdateAction<Cart> actionToAddProductInfoFieldsInCustomLineItem(final CustomLineItem customLineItem,
+                                                                                   final Map<String, Object> productInfo) {
+        return SetCustomLineItemCustomType.ofTypeKeyAndObjects(CUSTOM_TYPE_KEY, productInfo, customLineItem.getId());
+    }
+
+    private static Map<String, Object> extractInfoFromLineItem(final LineItem lineItem) {
+        final Map<String, Object> productInfo = new HashMap<>();
+        productInfo.put(PRODUCT_ID_FIELD, lineItem.getProductId());
+        productInfo.put(PRODUCT_SLUG_FIELD, lineItem.getProductSlug());
+        productInfo.put(PRODUCT_SKU_FIELD, lineItem.getVariant().getSku());
+        return productInfo;
     }
 
     private static Cart addProductToCart(final SphereClient client, final Cart cart, final ProductProjection product, final int quantity) {
@@ -96,27 +139,17 @@ public class Main {
 
     private static Type createTypeForExtendedCustomLineItem(final SphereClient client) {
         // This is only executed (or created) once, this doesn't need to be here in a real example
-        final PagedQueryResult<Type> result = execute(client, TypeQuery.of().withPredicates(type -> type.key().is(CUSTOM_TYPE_KEY)));
-
-        if (result.getResults().isEmpty()) {
-            final TypeDraft typeDraft = TypeDraftBuilder.of(CUSTOM_TYPE_KEY, LocalizedString.of(ENGLISH, "CustomLineItem with product"), asSet("custom-line-item"))
-                    .fieldDefinitions(asList(
-                            FieldDefinition.of(StringType.of(), PRODUCT_ID_FIELD, LocalizedString.of(ENGLISH, "Product ID"), true, SINGLE_LINE),
-                            FieldDefinition.of(LocalizedStringType.of(), PRODUCT_SLUG_FIELD, LocalizedString.of(ENGLISH, "Product Slug"), true, SINGLE_LINE),
-                            FieldDefinition.of(StringType.of(), PRODUCT_SKU_FIELD, LocalizedString.of(ENGLISH, "SKU"), true, SINGLE_LINE)))
-                    .build();
-            return execute(client, TypeCreateCommand.of(typeDraft));
-        } else {
-            return result.getResults().get(0);
-        }
+        final TypeDraft typeDraft = TypeDraftBuilder.of(CUSTOM_TYPE_KEY, LocalizedString.of(ENGLISH, "CustomLineItem with product"), asSet("custom-line-item"))
+                .fieldDefinitions(asList(
+                        FieldDefinition.of(StringType.of(), PRODUCT_ID_FIELD, LocalizedString.of(ENGLISH, "Product ID"), true, SINGLE_LINE),
+                        FieldDefinition.of(LocalizedStringType.of(), PRODUCT_SLUG_FIELD, LocalizedString.of(ENGLISH, "Product Slug"), true, SINGLE_LINE),
+                        FieldDefinition.of(StringType.of(), PRODUCT_SKU_FIELD, LocalizedString.of(ENGLISH, "SKU"), true, SINGLE_LINE)))
+                .build();
+        return execute(client, TypeCreateCommand.of(typeDraft));
     }
 
     private static String getCustomLineItemInCart(final Cart cartWithCustomLineItem) {
         return cartWithCustomLineItem.getCustomLineItems().get(0).getId(); // TODO find nicely by slug
-    }
-
-    private static LineItem getLineItemInCart(final Cart cart) {
-        return cart.getLineItems().get(0);
     }
 
     private static Money getFinalPriceFromSap() {
@@ -133,6 +166,11 @@ public class Main {
         final Properties prop = new Properties();
         prop.load(Main.class.getClassLoader().getResourceAsStream("dev.properties"));
         return prop;
+    }
+
+    private static void cleanUpProject(final SphereClient client, final Type typeForCustomLineItems, final Cart cartWithProductAndPrice) {
+        execute(client, CartDeleteCommand.of(cartWithProductAndPrice));
+        execute(client, TypeDeleteCommand.of(typeForCustomLineItems));
     }
 
     private static <T> T execute(final SphereClient client, final SphereRequest<T> sphereRequest) {
