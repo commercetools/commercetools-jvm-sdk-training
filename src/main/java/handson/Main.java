31be33e086fbf0b1f09a9d7dca547aa4cd256cbe
Laura Luiz <laura.luiz@commercetools.de>
2016-08-01 18:27:26 +0200

    Move client creation to another method and use blocking client instead

--- src/main/java/handson/Main.java
+++ src/main/java/handson/Main.java
@@ -1,28 +1,34 @@
 package handson;
 
+import io.sphere.sdk.client.BlockingSphereClient;
 import io.sphere.sdk.client.SphereClient;
 import io.sphere.sdk.client.SphereClientConfig;
 import io.sphere.sdk.client.SphereClientFactory;
 import io.sphere.sdk.products.queries.ProductProjectionQuery;
 
 import java.io.IOException;
+import java.time.Duration;
 import java.util.Properties;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        final Properties prop = loadCommercetoolsPlatformProperties();
-        final String projectKey = prop.getProperty("projectKey");
-        final String clientId = prop.getProperty("clientId");
-        final String clientSecret = prop.getProperty("clientSecret");
-        final SphereClientConfig clientConfig = SphereClientConfig.of(projectKey, clientId, clientSecret);
-        try(final SphereClient client = SphereClientFactory.of().createClient(clientConfig)) {
+        try(final SphereClient client = createSphereClient()) {
             System.err.println(client.execute(ProductProjectionQuery.ofCurrent()).toCompletableFuture().join());
         }
     }
 
-    private static Properties loadCommercetoolsPlatformProperties() throws IOException {
+    private static BlockingSphereClient createSphereClient() throws IOException {
+        final SphereClientConfig clientConfig = loadCommercetoolsPlatformClientConfig();
+        final SphereClient client = SphereClientFactory.of().createClient(clientConfig);
+        return BlockingSphereClient.of(client, Duration.ofMinutes(1));
+    }
+
+    private static SphereClientConfig loadCommercetoolsPlatformClientConfig() throws IOException {
         final Properties prop = new Properties();
         prop.load(Main.class.getClassLoader().getResourceAsStream("dev.properties"));
-        return prop;
+        final String projectKey = prop.getProperty("projectKey");
+        final String clientId = prop.getProperty("clientId");
+        final String clientSecret = prop.getProperty("clientSecret");
+        return SphereClientConfig.of(projectKey, clientId, clientSecret);
     }
 }
