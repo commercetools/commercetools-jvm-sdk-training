8f308d8d59e3a4ab0b783bab8420dcf5a2196ca6
Houssein Ben Amor <houssein.benamor@commercetools.de>
2017-02-28 09:38:28 +0100

    initial commit

--- src/main/java/handson/Main.java
+++ src/main/java/handson/Main.java
@@ -1,202 +1,61 @@
 package handson;
 
 import com.neovisionaries.i18n.CountryCode;
-import io.sphere.sdk.carts.Cart;
-import io.sphere.sdk.carts.CartDraft;
-import io.sphere.sdk.carts.commands.CartCreateCommand;
-import io.sphere.sdk.carts.commands.CartUpdateCommand;
-import io.sphere.sdk.carts.commands.updateactions.AddLineItem;
-import io.sphere.sdk.carts.commands.updateactions.SetShippingAddress;
-import io.sphere.sdk.carts.queries.CartByIdGet;
 import io.sphere.sdk.client.BlockingSphereClient;
 import io.sphere.sdk.models.Address;
+import io.sphere.sdk.models.AddressBuilder;
 import io.sphere.sdk.orders.Order;
-import io.sphere.sdk.orders.commands.OrderFromCartCreateCommand;
-import io.sphere.sdk.orders.commands.OrderUpdateCommand;
-import io.sphere.sdk.orders.commands.updateactions.SetCustomType;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.concurrent.ExecutionException;
 
+import static handson.Commands.*;
+import static handson.Project.*;
 import static handson.Utils.createSphereClient;
-import static handson.Utils.printCart;
-import static io.sphere.sdk.models.DefaultCurrencyUnits.EUR;
 
-public class Main extends ProjectInitialization {
+public class Main {
 
-    public static void main(String[] args) throws IOException {
-        try (final BlockingSphereClient client = createSphereClient()) {
+    private static final Address address = AddressBuilder.of(CountryCode.DE).build();
 
-            /**
-             * TASKS:
-             *
-             * - Add attribute values for the products:
-             *      - Modify "createProduct" method to pass `String color` and `boolean handmade`
-             *      - Add these values to the master variant draft
-             *
-             * - Search products, by the following criteria:
-             *      - Sort them by attribute color ascending
-             *      - Additionally sort them by price descending (multi sorting)
-             *      - Try using fulltext search and search for a value
-             *
-             * - Display information from master variant products (remember: staged versions!):
-             *      - Display SKU
-             *      - Display price
-             *      - Display attribute "color" (string) and "handmade" (boolean)
-             */
+    public static void main(String[] args) throws IOException, ExecutionException, InterruptedException {
 
-            // Set up project with some products
+        try (final BlockingSphereClient client = createSphereClient()){
+
             setUpProject(client);
 
-            // The online shop creates an order
-            final Order order = onlineShopSide(client);
+            final Order order = onlineShop(client);
 
-            // The microservice listens to orders and sets fraud score
-            final Order orderWithFraudScore = microServiceSide(client, order);
+            // .. the order in a microservice, e.g. send confirmation email to the customer.
 
-            // Clean up project to avoid conflicts for next execution
-            cleanUpProject(client, orderWithFraudScore);
+            cleanUpProject(client, product, taxCategory, cart, order);
         }
     }
 
     /**
-     * What the online shop does.
-     * In particular:
-     * - Creates a cart
-     * - Fills the cart with line items
-     * - Sets the shipping address of the cart
-     * - Places an order with the cart
+     * Online ship side:
+     * - queries a cart, or creates one if none is available
+     * - add product to cart
+     * - sets a shipping address to cart
+     * - creates order from cart
      * @param client CTP client
-     * @return the created order at the end of the online shop execution
+     * @return The created order
      */
-    private static Order onlineShopSide(final BlockingSphereClient client) {
-        // We create a cart at some moment, e.g. when the customer logs in
-        final Cart cart = createCart(client);
-        printCart(cart, "Empty cart created");
+    private static Order onlineShop(final BlockingSphereClient client){
+        cart = queryFirstCart(client);
+        System.out.println("Cart with id " + cart.getId() + " is queried/created");
 
-        // We keep the cart ID (typically in session) for future requests
-        final String sessionCartId = cart.getId();
+        cart = addProductToCart(client, product.getId(), cart, 1L);
+        System.out.println("Product with id " + product.getId() + " is added to cart.");
 
-        // Many pages are clicked...
-
-        // First we fetch our cart from the session
-        final Cart cartFetchedFromSession = fetchCartById(client, sessionCartId);
-
-        // When the user clicks on "add to cart", we receive the following data from the request:
-        final int quantity = 3;
-        final String productId = shirt.getId();
-        final Integer variantId = shirt.getMasterData().getStaged().getMasterVariant().getId();
-
-        // Then we add a product to the cart, which contains a non-customized price
-        final Cart cartWithLineItem = addProductToCart(client, cartFetchedFromSession, productId, variantId, quantity);
-        printCart(cartWithLineItem, "Added product to cart");
-
-        // When the user introduces the shipping address, we receive the country for shipping (minimum info necessary):
-        final Address shippingAddress = Address.of(CountryCode.DE);
-
-        // Then we set the shipping address to the cart
-        final Cart cartWithShippingAddress = setShippingAddress(client, cartWithLineItem, shippingAddress);
+        cart = setShippingAddress(client, address, cart);
+        System.out.println("Set address to cart with id " + cart.getId());
 
         // Checkout process continues...
 
         // When it is confirmed and paid, we create the order
-        final Order order = createOrderFromCart(client, cartWithShippingAddress);
-        printCart(order, "Created order");
+        Order order = createOrderFromCart(client, cart);
+        System.out.println("Order with id " + order.getId() + " is added to cart with id " + cart.getId());
+
         return order;
-    }
-
-    /**
-     * What the microservice is doing.
-     * In particular:
-     * - Sets the fraud score to the order
-     * @param client CTP client
-     * @param order the recently created Order (represents the order obtained via Messages)
-     * @return the updated order with the Fraud Score set
-     */
-    private static Order microServiceSide(final BlockingSphereClient client, final Order order) {
-        // On the other hand, the microservice listens to an order created message...
-
-        // When a new order is detected, we request a fraud detection external system for a score:
-        final double fraudScore = 7;
-
-        // Then we update the cart with the given fraud score
-        final Order orderWithFraudScore = setFraudScore(client, order, fraudScore);
-        printCart(orderWithFraudScore, "Updated order with fraud score");
-        return orderWithFraudScore;
-    }
-
-    /**
-     * Sets the given fraud score to the Order.
-     * @param client CTP client
-     * @param order the order to which the fraud score should be set
-     * @param fraudScore the fraud score for the order
-     * @return the updated order with the given fraud score set
-     */
-    private static Order setFraudScore(final BlockingSphereClient client, final Order order, final double fraudScore) {
-        final Map<String, Object> values = new HashMap<>();
-        values.put(FRAUD_SCORE_FIELD, fraudScore);
-        final SetCustomType setCustomType = SetCustomType.ofTypeKeyAndObjects(ORDER_WITH_FRAUD_SCORE_TYPE_KEY, values);
-        final OrderUpdateCommand updateCommand = OrderUpdateCommand.of(order, setCustomType);
-        return client.executeBlocking(updateCommand);
-    }
-
-    /**
-     * Creates an empty Cart.
-     * @param client CTP client
-     * @return the created cart
-     */
-    private static Cart createCart(final BlockingSphereClient client) {
-        final CartCreateCommand createCommand = CartCreateCommand.of(CartDraft.of(EUR));
-        return client.executeBlocking(createCommand);
-    }
-
-    /**
-     * Fetches the Cart identified by the given ID.
-     * @param client CTP client
-     * @param cartId identifies the cart
-     * @return the cart associated with the given ID
-     */
-    private static Cart fetchCartById(final BlockingSphereClient client, final String cartId) {
-        final CartByIdGet fetchQuery = CartByIdGet.of(cartId);
-        return client.executeBlocking(fetchQuery);
-    }
-
-    /**
-     * Adds the given Product/Variant combination with the given quantity to the Cart.
-     * @param client CTP client
-     * @param cart the cart to which the product should be added
-     * @param productId the identifier of the product
-     * @param variantId the identifier of the variant
-     * @param quantity the amount of product units to add
-     * @return the updated cart with the product as a line item
-     */
-    private static Cart addProductToCart(final BlockingSphereClient client, final Cart cart, final String productId,
-                                         final int variantId, final int quantity) {
-        final CartUpdateCommand updateCommand = CartUpdateCommand.of(cart, AddLineItem.of(productId, variantId, quantity));
-        return client.executeBlocking(updateCommand);
-    }
-
-    /**
-     * Sets the given shipping address to the Cart.
-     * @param client CTP client
-     * @param cart the cart to which the shipping address is set
-     * @param shippingAddress the shipping address to associate to the cart
-     * @return the updated cart with the shipping address
-     */
-    private static Cart setShippingAddress(final BlockingSphereClient client, final Cart cart, final Address shippingAddress) {
-        final CartUpdateCommand updateCommand = CartUpdateCommand.of(cart, SetShippingAddress.of(shippingAddress));
-        return client.executeBlocking(updateCommand);
-    }
-
-    /**
-     * Create an Order out of the given Cart.
-     * @param client CTP client
-     * @param cart the cart to be transformed into an order
-     * @return the created Order
-     */
-    private static Order createOrderFromCart(final BlockingSphereClient client, final Cart cart) {
-        final OrderFromCartCreateCommand createCommand = OrderFromCartCreateCommand.of(cart);
-        return client.executeBlocking(createCommand);
     }
 }
--- src/main/java/handson/Utils.java
+++ src/main/java/handson/Utils.java
@@ -1,46 +1,25 @@
 package handson;
 
-import io.sphere.sdk.carts.CartLike;
 import io.sphere.sdk.client.BlockingSphereClient;
 import io.sphere.sdk.client.SphereClientConfig;
 import io.sphere.sdk.client.SphereClientFactory;
 
-import java.io.IOException;
 import java.time.Duration;
-import java.util.Optional;
-import java.util.Properties;
 
-import static handson.ProjectInitialization.FRAUD_SCORE_FIELD;
+class Utils {
 
-public class Utils {
+    /**
+     * Creates a blocking sphere client
+     * @return Sphere client
+     */
+    static BlockingSphereClient createSphereClient() {
+        final String projectKey = "fyayc-hou-42";
+        final String clientId = "Olmu4RPsFJZvN82npylM6jII";
+        final String clientSecret = "VdELOheKjPbprfWrbG5wrYUZZ01ojARr";
 
-    public static void printCart(final CartLike<?> cartLike, final String message) {
-        System.err.println("");
-        System.err.println(message + ":");
-        final int quantity = cartLike.getLineItems().size();
-        System.err.println("Line items: " + quantity);
-        cartLike.getLineItems().forEach(lineItem -> System.err.println("  " + lineItem));
-        if (cartLike.getCustom() != null) {
-            Optional.ofNullable(cartLike.getCustom().getFieldAsLong(FRAUD_SCORE_FIELD))
-                    .ifPresent(fraudScore -> System.err.println("Fraud score: " + fraudScore));
-        } else {
-            System.err.println("Fraud score: <Unavailable>");
-        }
-    }
+        final SphereClientConfig clientConfig = SphereClientConfig.of(projectKey, clientId, clientSecret);
 
-    public static BlockingSphereClient createSphereClient() throws IOException {
-        final SphereClientConfig clientConfig = loadCommercetoolsPlatformClientConfig();
         return BlockingSphereClient.of(SphereClientFactory.of().createClient(clientConfig), Duration.ofMinutes(1));
-    }
-
-
-    private static SphereClientConfig loadCommercetoolsPlatformClientConfig() throws IOException {
-        final Properties prop = new Properties();
-        prop.load(Main.class.getClassLoader().getResourceAsStream("dev.properties"));
-        final String projectKey = prop.getProperty("projectKey");
-        final String clientId = prop.getProperty("clientId");
-        final String clientSecret = prop.getProperty("clientSecret");
-        return SphereClientConfig.of(projectKey, clientId, clientSecret);
     }
 
 }
