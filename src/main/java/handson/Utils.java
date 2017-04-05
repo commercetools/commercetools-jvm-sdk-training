8130194fa162c9efe8e0bf9713f1f6a34fbc7093
Houssein Ben Amor <houssein.benamor@commercetools.de>
2017-04-05 11:47:12 +0200

    add properties to utils.java update TODOs in utils.java

--- src/main/java/handson/Utils.java
+++ src/main/java/handson/Utils.java
@@ -2,25 +2,44 @@ package handson;
 
 import io.sphere.sdk.client.BlockingSphereClient;
 import io.sphere.sdk.client.SphereClientConfig;
-import io.sphere.sdk.client.SphereClientFactory;
 
-import java.time.Duration;
+import java.io.IOException;
+import java.util.Properties;
 
 class Utils {
 
     /**
      * Creates a blocking sphere client
      * @return Sphere client
+     * @throws IOException
      */
-    static BlockingSphereClient createSphereClient() {
-        //TODO 1.1.1. Get the project info from the Admin Center
-        final String projectKey = "";
-        final String clientId = "";
-        final String clientSecret = "";
+    static BlockingSphereClient createSphereClient() throws IOException {
+
+        final SphereClientConfig clientConfig = loadCTPClientConfig();
+
+        //TODO 1.1.3. Create the client
+        return null;
+    }
+
+    /**
+     * Sets a sphere client configuration
+     * @return sphere client configuration
+     * @throws IOException
+     */
+    private static SphereClientConfig loadCTPClientConfig() throws IOException {
+        final Properties prop = new Properties();
+        prop.load(Main.class.getClassLoader()
+                .getResourceAsStream("dev.properties"));
+
+        final String projectKey = prop.getProperty("projectKey");
+        final String clientId = prop.getProperty("clientId");
+        final String clientSecret = prop.getProperty("clientSecret");
+
+        // For the US, uncomment following lines
+        // final String authUrl = prop.getProperty("authUrl");
+        // final String apiUrl = prop.getProperty("apiUrl");
 
         //TODO 1.1.2. Create the configuration for the sphere client
-
-        //TODO 1.1.3. Create the client and return it
         return null;
     }
 
