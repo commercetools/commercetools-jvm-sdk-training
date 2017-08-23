6d8c5c61b9fe9b7adedf252b3af028f92eee4c6f
mkoester <matthias.koester@commercetools.de>
2017-08-23 13:36:07 +0200

    Add further methods.

--- src/main/java/handson/CustomerService.java
+++ src/main/java/handson/CustomerService.java
@@ -2,9 +2,7 @@ package handson;
 
 import io.sphere.sdk.client.SphereClient;
 import io.sphere.sdk.customers.*;
-import io.sphere.sdk.customers.commands.CustomerCreateCommand;
-import io.sphere.sdk.customers.commands.CustomerCreateEmailTokenCommand;
-import io.sphere.sdk.customers.commands.CustomerVerifyEmailCommand;
+import io.sphere.sdk.customers.commands.*;
 
 import java.util.concurrent.CompletionStage;
 
@@ -34,4 +32,14 @@ public class CustomerService {
     public CompletionStage<CustomerToken> createEmailVerificationToken(final Customer customer) {
         return client.execute(CustomerCreateEmailTokenCommand.of(customer, 5));
     }
+
+
+    public CompletionStage<CustomerToken> createPasswordResetToken(final String email) {
+        return client.execute(CustomerCreatePasswordTokenCommand.of(email));
+    }
+
+    public CompletionStage resetPassword(final CustomerToken passwordResetToken, final String newPassword) {
+        return client.execute(CustomerPasswordResetCommand.ofTokenAndPassword(passwordResetToken.getValue(), newPassword));
+    }
+
 }
