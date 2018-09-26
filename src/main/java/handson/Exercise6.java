739c693166f3c71183b0968470b25d146c50a3ee
Hesham Massoud <hesham.massoud@commercetools.de>
2018-09-26 09:36:51 +0200

    Remove redundant line.

--- src/main/java/handson/Exercise6.java
+++ src/main/java/handson/Exercise6.java
@@ -46,8 +46,6 @@ public class Exercise6 {
             final CompletableFuture<PagedQueryResult<ProductProjection>> productsOnSaleResult =
                     productQueryService.findProductsWithCategory(Locale.ENGLISH, "Sale")
                             .toCompletableFuture();
-                productQueryService.findProductsWithCategory(Locale.ENGLISH, "Sale")
-                                   .toCompletableFuture();
 
             final PagedQueryResult<ProductProjection> productProjectionPagedQueryResult = productsOnSaleResult.get();
             final ProductProjection productProjection = productProjectionPagedQueryResult.getResults().get(0);
